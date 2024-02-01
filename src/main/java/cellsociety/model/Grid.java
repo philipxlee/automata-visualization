30f2930c32efaf46775de3afa362f1151f9e3b73
Abhishek Chataut <ac802@duke.edu>
2024-01-31 22:28:05 -0500

    reformmated again * 2

--- src/main/java/cellsociety/model/Grid.java
+++ src/main/java/cellsociety/model/Grid.java
@@ -7,8 +7,8 @@ import java.util.Map;
 
 public class Grid {
 
-  private final int ROW;
-  private final int COL;
+  private final int row;
+  private final int col;
 
   private final Cell[][] cellGrid;
   private Map<Cell, List<Cell>> cellNeighbors;
@@ -21,10 +21,10 @@ public class Grid {
    * @param col The number of columns in the grid.
    */
   public Grid(int row, int col) {
-    this.ROW = row;
-    this.COL = col;
+    this.row = row;
+    this.col = col;
     this.cellNeighbors = new HashMap<>();
-    this.cellGrid = new Cell[ROW][COL];
+    this.cellGrid = new Cell[row][col];
     initializeGridCells();
   }
 
@@ -35,9 +35,9 @@ public class Grid {
    * updated with these new states.
    */
   public void computeNextGenerationGrid() {
-    Cell[][] tempGrid = new Cell[ROW][COL];
-    for (int i = 0; i < ROW; i++) {
-      for (int j = 0; j < COL; j++) {
+    Cell[][] tempGrid = new Cell[row][col];
+    for (int i = 0; i < row; i++) {
+      for (int j = 0; j < col; j++) {
         List<Cell> neighbors = cellNeighbors.get(cellGrid[i][j]);
         String newState = determineNewState(cellGrid[i][j], neighbors);
         tempGrid[i][j] = new Cell(i, j, newState);
@@ -48,8 +48,8 @@ public class Grid {
 
   private void initializeGridCells() {
     char[][] gridState = getGridConfiguration();
-    for (int i = 0; i < ROW; i++) {
-      for (int j = 0; j < COL; j++) {
+    for (int i = 0; i < row; i++) {
+      for (int j = 0; j < col; j++) {
         String state = getStateFromChar(gridState[i][j]); // placeholder
         Cell currentCell = new Cell(i, j, state);
         cellGrid[i][j] = currentCell;
@@ -65,8 +65,8 @@ public class Grid {
   }
 
   private void updateGridWithNewStates(Cell[][] tempGrid) {
-    for (int i = 0; i < ROW; i++) {
-      for (int j = 0; j < COL; j++) {
+    for (int i = 0; i < row; i++) {
+      for (int j = 0; j < col; j++) {
         String newState = tempGrid[i][j].getState();
         cellGrid[i][j].setState(newState);
       }
@@ -85,7 +85,7 @@ public class Grid {
   }
 
   private void addNeighborsWithinBounds(int newRow, int newCol, List<Cell> neighbors) {
-    if (newRow >= 0 && newRow < ROW && newCol >= 0 && newCol < COL) {
+    if (newRow >= 0 && newRow < row && newCol >= 0 && newCol < col) {
       Cell neighbor = cellGrid[newRow][newCol];
       neighbors.add(neighbor);
     }
