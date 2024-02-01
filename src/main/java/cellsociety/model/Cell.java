c926b29bb5a79ae36722a1523a9f97bf3b418007
Abhishek Chataut <ac802@duke.edu>
2024-01-31 22:21:19 -0500

    reformatted

--- src/main/java/cellsociety/model/Cell.java
+++ src/main/java/cellsociety/model/Cell.java
@@ -1,17 +1,23 @@
 package cellsociety.model;
 
 public class Cell {
-    private final int ROW;
-    private final int COL;
-    private String state;
 
-    public Cell (int row, int col, String state) {
-        this.ROW = row;
-        this.COL = col;
-        this.state = state;
-    }
+  private final int ROW;
+  private final int COL;
+  private String state;
 
-    public void setState(String state) { this.state = state; }
-    public String getState() { return this.state; }
+  public Cell(int row, int col, String state) {
+    this.ROW = row;
+    this.COL = col;
+    this.state = state;
+  }
+
+  public String getState() {
+    return this.state;
+  }
+
+  public void setState(String state) {
+    this.state = state;
+  }
 
 }
