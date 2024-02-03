d75b81e8678776ecc810cc73dc82ab8eb79950ba
SAAD HAKIM <SH604@duke.edu>
2024-02-03 12:03:40 -0500

    testing adding Config

--- src/main/java/cellsociety/Config/Config.java
+++ src/main/java/cellsociety/Config/Config.java
@@ -1,22 +1,11 @@
-package cellsociety;
+package cellsociety.Config;
 
 
 import java.util.HashMap;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import org.xml.sax.ErrorHandler;
-import org.xml.sax.SAXException;
-import org.xml.sax.SAXParseException;
-import org.xml.sax.helpers.*;
 import java.io.File;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
 import org.w3c.dom.Document;
-import org.w3c.dom.DocumentType;
-import org.w3c.dom.Entity;
-import org.w3c.dom.NamedNodeMap;
-import org.w3c.dom.Node;
 
 public class Config {
     String simulationType;
