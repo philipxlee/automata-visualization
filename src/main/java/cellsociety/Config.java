7eccc6d001da4ebad944b67944a68e32f3196ab4
SAAD HAKIM <SH604@duke.edu>
2024-02-03 11:53:16 -0500

    added instance variables for config

--- src/main/java/cellsociety/Config.java
+++ src/main/java/cellsociety/Config.java
@@ -1,42 +1,45 @@
 package cellsociety;
 
-import org.w3c.dom.Document;
-import org.w3c.dom.NodeList;
-import org.w3c.dom.Element;
-import org.xml.sax.SAXException;
+
+import java.util.HashMap;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+import org.xml.sax.helpers.*;
 import java.io.File;
-import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import org.w3c.dom.Document;
+import org.w3c.dom.DocumentType;
+import org.w3c.dom.Entity;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
 
 public class Config {
-    String gameType;
-    String gameTitle;
+    String simulationType;
+    String simulationTitle;
+    String author;
+    String description;
+
+    // ideally combine them in a tuple
+    Double width;
+    Double height;
+    String filePath;
+    HashMap<String, Integer> parameter;
 
     Config() {
 
     }
 
-    public void loadXMLFile(String path)  {
+    public void loadXMLFile(String path) throws Exception {
 
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        try {
-            DocumentBuilder builder = factory.newDocumentBuilder();
-            try {
-                Document doc = builder.parse(new File(path));
-                Element sim = doc.getDocumentElement();
-                NodeList mod = sim.getElementsByTagName("model");
-                Element typ = (Element) mod.item(0);
-                System.out.println(typ.getTagName());
-            } catch (SAXException e) {
-                throw new RuntimeException(e);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        } catch (ParserConfigurationException e) {
-            throw new RuntimeException(e);
-        }
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        DocumentBuilder db = dbf.newDocumentBuilder();
+        Document doc = db.parse(new File(path));
+
 
 
 
@@ -55,7 +58,7 @@ public class Config {
 
     }
 
-    public static void main(String args[]) {
+    public static void main(String args[]) throws Exception {
         Config myConfig = new Config();
         myConfig.loadXMLFile("C:\\Users\\Ashitaka\\CS308\\cellsociety_team03\\test.xml");
 
