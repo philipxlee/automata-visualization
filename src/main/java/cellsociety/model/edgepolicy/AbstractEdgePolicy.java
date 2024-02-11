7084afb2df5361a55478691813032c6e83a6a5ac
Philip Lee <kl445@duke.edu>
2024-02-11 18:14:32 -0500

    Refactored line to be shorter than 100 characters

--- src/main/java/cellsociety/model/edgepolicy/AbstractEdgePolicy.java
+++ src/main/java/cellsociety/model/edgepolicy/AbstractEdgePolicy.java
@@ -6,22 +6,22 @@ import java.util.List;
 
 public abstract class AbstractEdgePolicy<T extends Cell> implements EdgePolicy<T> {
 
-  public List<T> getNeighbors(int row, int col, T[][] cellGrid) {
+  public List<T> getNeighbors(int row, int col, T[][] grid) {
     List<T> neighbors = new ArrayList<>();
     int[][] directions = {{-1, 0}, {-1, -1}, {0, 1}, {1, 1}, {1, 0}, {1, -1}, {0, -1}, {-1, 1}};
     for (int[] direction : directions) {
       int newRow = row + direction[0];
       int newCol = col + direction[1];
-      if (isInBounds(newRow, newCol, cellGrid) && isValidNeighbor(row, col, newRow, newCol, cellGrid)) {
-        neighbors.add(cellGrid[newRow][newCol]);
+      if (isInBounds(newRow, newCol, grid) && isValidNeighbor(row, col, newRow, newCol, grid)) {
+        neighbors.add(grid[newRow][newCol]);
       }
     }
     return neighbors;
   }
 
-  public boolean isInBounds(int newRow, int newCol, T[][] cellGrid) {
-    return newRow >= 0 && newRow < cellGrid.length && newCol >= 0 && newCol < cellGrid[0].length;
+  public boolean isInBounds(int newRow, int newCol, T[][] grid) {
+    return newRow >= 0 && newRow < grid.length && newCol >= 0 && newCol < grid[0].length;
   }
 
-  public abstract boolean isValidNeighbor(int row, int col, int newRow, int newCol, T[][] cellGrid);
+  public abstract boolean isValidNeighbor(int row, int col, int newRow, int newCol, T[][] grid);
 }
