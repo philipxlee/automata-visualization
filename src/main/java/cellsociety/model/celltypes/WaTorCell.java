f7584041e64c6094ac7d9a1d3b706b46ee75a2c4
Philip Lee <kl445@duke.edu>
2024-02-10 19:12:36 -0500

    CELL-04: Updated WaTor functionality to work by designing simultaneous processing

--- src/main/java/cellsociety/model/celltypes/WaTorCell.java
+++ src/main/java/cellsociety/model/celltypes/WaTorCell.java
@@ -6,105 +6,92 @@ import cellsociety.model.CellStates;
 public class WaTorCell extends Cell {
 
   private static final String SHARK = CellStates.SHARK.name();
-  private static final int SHARK_BREED_TIME = 7;
-  private static final int FISH_BREED_TIME = 5;
-  private static final int SHARK_STARTING_ENERGY = 10;
+  private static final String FISH = CellStates.FISH.name();
+  private static final String EMPTY = CellStates.EMPTY.name();
+  public static final int SHARK_BREED_TIME = 7; // Example value, adjust as necessary
+  public static final int FISH_BREED_TIME = 5; // Example value, adjust as necessary
+  public static final int SHARK_STARTING_ENERGY = 10; // Example value, adjust as necessary
 
   private int breedTime = 0;
   private int energy = SHARK_STARTING_ENERGY;
-  private boolean eaten = false;
+  private boolean isEaten = false;
 
-  /**
-   * Create a new cell with the given state at the given row and column
-   *
-   * @param row
-   * @param col
-   * @param state
-   */
   public WaTorCell(int row, int col, String state) {
     super(row, col, state);
+    if (state.equals(SHARK)) {
+      this.energy = SHARK_STARTING_ENERGY;
+    }
   }
 
-  /**
-   * Get the breed time of the cell
-   *
-   * @return the breed time of the cell
-   */
   public int getBreedTime() {
-    return this.breedTime;
+    return breedTime;
   }
 
-  /**
-   * Set the breed time of the cell
-   *
-   * @param time the breed time of the cell
-   */
-  public void setBreedTime(int time) {
-    this.breedTime = time;
+  public void setBreedTime(int breedTime) {
+    this.breedTime = breedTime;
   }
-
-  /**
-   * Set the energy of the cell
-   *
-   * @param energy the energy of the cell
-   */
-  public void setEnergy(int energy) {
-    this.breedTime = energy;
-  }
-
-  /**
-   * Get the energy of the cell
-   *
-   * @return the energy of the cell
-   */
 
   public int getEnergy() {
-    return this.energy;
+    return energy;
   }
 
-  /**
-   * Checks if the animal can reproduce
-   *
-   * @param animal the animal to check
-   * @return a boolean if the animal can reproduce
-   */
-  public boolean canReproduce(String animal) {
-    return (animal.equals(SHARK)) ? breedTime == SHARK_BREED_TIME : breedTime == FISH_BREED_TIME;
+  public void setEnergy(int energy) {
+    this.energy = energy;
   }
 
-  /**
-   * Checks if the shark has starved
-   *
-   * @return a boolean if the shark has starved
-   */
-  public boolean sharkStarve() {
-    return this.energy == 0;
+  public boolean isEaten() {
+    return isEaten;
   }
 
-  /**
-   * Checks if the animal has been eaten
-   *
-   * @return a boolean if the animal has been eaten
-   */
-  public boolean getIsEaten() {
-    return this.eaten;
+  public void setEaten(boolean eaten) {
+    this.isEaten = eaten;
   }
 
-  /**
-   * Sets the animal to be eaten
-   *
-   * @param eaten a boolean if the animal has been eaten
-   */
-  public void setIsEaten(boolean eaten) {
-    this.eaten = eaten;
-  }
-
-  /**
-   * Resets the animal
-   */
   public void resetAnimal() {
-    this.energy = 0;
     this.breedTime = 0;
-    this.eaten = false;
+    this.energy = (this.getState().equals(SHARK)) ? SHARK_STARTING_ENERGY : 0;
+    this.isEaten = false;
+  }
+
+  public void eatFish(WaTorCell fishCell) {
+    this.energy += 2; // Example value, adjust as necessary
+    fishCell.setEaten(true);
+  }
+
+  public void decreaseEnergy() {
+    this.energy -= 1;
+  }
+
+  public boolean sharkStarve() {
+    return this.energy <= 0;
+  }
+
+  public void increaseBreedTime() {
+    this.breedTime++;
+  }
+
+  public void resetBreedTime() {
+    this.breedTime = 0;
+  }
+
+  public boolean canReproduce(String type) {
+    if (type.equals(SHARK)) {
+      return breedTime >= SHARK_BREED_TIME;
+    } else if (type.equals(FISH)) {
+      return breedTime >= FISH_BREED_TIME;
+    }
+    return false;
+  }
+
+  public void die() {
+    this.setState(EMPTY);
+    resetAnimal();
+  }
+
+  public void become(String state) {
+    this.setState(state);
+    if (state.equals(SHARK)) {
+      setEnergy(SHARK_STARTING_ENERGY);
+    }
   }
 }
