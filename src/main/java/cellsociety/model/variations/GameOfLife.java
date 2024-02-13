f8d6789f5d61f7d1d69487cdcfcb4b2e6bdedcfd
Abhishek Chataut <ac802@duke.edu>
2024-02-12 23:30:30 -0500

    fixed import

--- src/main/java/cellsociety/model/variations/GameOfLife.java
+++ src/main/java/cellsociety/model/variations/GameOfLife.java
@@ -1,6 +1,5 @@
 package cellsociety.model.variations;
 
-import static jdk.internal.joptsimple.internal.Strings.EMPTY;
 
 import cellsociety.model.CellStates;
 import cellsociety.model.Simulation;
@@ -11,6 +10,7 @@ public class GameOfLife implements Simulation<BasicCell> {
 
   private static final String ALIVE = CellStates.ALIVE.name();
   private static final String DEAD = CellStates.DEAD.name();
+  private static final String EMPTY = CellStates.EMPTY.name();
 
   /**
    * Creates a new GameOfLifeCell with specified row, column, and state.
